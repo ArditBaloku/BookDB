9886107069b4b666ac27d864e082633722c1c81a
Ardit Baloku <43915132+ArditBaloku@users.noreply.github.com>
2019-12-16 16:56:44 +0100

    Update, delete book model

--- models/book.js
+++ models/book.js
@@ -9,6 +9,25 @@ const insertBook = async (title, summary, isbn, authorName, authorSurname) => {
   return await db.executeAsync(sql, [title, summary, isbn, authorName, authorSurname], conn)
 }
 
+const updateBook = async (bookId, title, summary, isbn, authorName, authorSurname) => {
+  const sql = `UPDATE BOOKS SET
+  TITLE=':title',
+  SUMMARY=':summary',
+  ISBN=ISBN(':isbn'),
+  AUTHOR=AUTHOR(':authorName',':authorSurname')
+  WHERE BOOKID=':bookId'`
+  const conn = await db.getConnect()
+  return await db.executeAsync(sql, [bookId, title, summary, isbn, authorName, authorSurname], conn)
+}
+
+const deleteBook = async (bookId) => {
+  const sql = `DELETE FROM BOOKS WHERE BOOKID=:bookId`
+  const conn = await db.getConnect()
+  return await db.executeAsync(sql, [bookId], conn)
+}
+
 module.exports = {
-  insertBook
+  insertBook,
+  updateBook,
+  deleteBook
 }
